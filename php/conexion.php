b2ce7192dec7a7935b044314ac831795823e56b4
Cesar2027365 <cesar.herrera2027@adc.superate.org.sv>
2026-06-01 19:39:24 -0600

    Completo iniciar sesion y cerrarla

--- php/conexion.php
+++ php/conexion.php
@@ -1,13 +1,14 @@
-<?php 
-    $host = "localhost"; 
-    $user = "root"; 
-    $password = ""; 
-    $dbname = "coink"; 
-    $conn = new mysqli($host, $user, $password, "coink");
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "db-coink";
 
-    if ($conn->connect_error) { 
-    die("Error de conexión: " . $conn->connect_error); 
-    } 
+$conn = new mysqli($host, $user, $password, $dbname);
 
-    $conn->set_charset("utf8mb4"); 
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
 ?>
