920ef10d5feb43664e06c2c87e67c6ea84487371
Cesar2027365 <cesar.herrera2027@adoc.superate.org.sv>
2026-06-05 17:05:32 -0600

    nuevo nombre database

--- php/conexion.php
+++ php/conexion.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $password = "";
-$dbname = "db-coink";
+$dbname = "coink";
 
 $conn = new mysqli($host, $user, $password, $dbname);
 
@@ -11,4 +11,5 @@ if ($conn->connect_error) {
 }
 
 $conn->set_charset("utf8mb4");
+
 ?>
