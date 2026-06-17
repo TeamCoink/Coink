b5ab57dd74d0768eda5794c43dc17ebfa48b4f9e
camii-29 <camila.hernandez2027@adoc.superate.org.sv>
2026-06-16 21:34:12 -0600

    Homepage is being fixed, not finished yet

---
+++ homepage.php
@@ -0,0 +1,66 @@
+<?php include 'components/navbar.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/homepage.css">
+    <link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+</head>
+<body>
+
+ <section class="hero">
+    <div class="hero-content">
+        <h1>
+            Aprende a <br> manejar tu dinero <br> y cumple
+            <div class="pink">tus sueños.</div>  
+        </h1>
+
+        <p>
+            Educación financiera sencilla y práctica para que tomes
+            mejores decisiones cada día.
+        </p>
+
+        <div class="hero-buttons">
+            <a href="metodos.php" class="btn-primary">Aprender</a>
+            <a href="login.php" class="btn-secondary">Unirme</a>
+        </div>
+    </div>
+
+    <div class="hero-image">
+        <img src="img/pigH.png" alt="Cerdito Coink" width="1000px">
+    </div>
+</section> 
+
+<section id="features">
+    <div class="feature">
+        <img src="img/feature1.png" alt="" width="140px">
+        <h3>Aprender</h3>
+        <p>Conceptos claros y metodos efectivos</p>
+    </div>
+
+    <div class="feature">
+        <img src="img/feature2.png" alt="" width="140px">
+        <h3>Planifica</h3>
+        <p>Organiza tus metas y finanzas propias</p>
+    </div>
+
+    <div class="feature">
+        <img src="img/feature3.png" alt="" width="140px">
+        <h3>Ahorra</h3>
+        <p>Pequeños hábitos, grandes cambios</p>
+    </div>
+
+    <div class="feature">
+        <img src="img/feature4.png" alt="" width="140px">
+        <h3>Invierte</h3>
+        <p>Haz crecer tu dinero de manera rapidas</p>
+    </div>
+
+</section>
+    
+</body>
+</html>
